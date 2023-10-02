8928232e96b32b84b9eaa620e6bd8e23cdf4f09d
egorn <egornich99@mail.ru>
2023-10-02 15:20:48 +0300

    Добавили функцию вычитания

--- py_project/main.py
+++ py_project/main.py
@@ -1,4 +1,9 @@
 from funcs import add_numbers
 
-result = add_numbers(5, 3)
-print(result)
+result_plus = add_numbers(5, 3)
+print(result_plus)
+
+from funcs import subtract_numbers
+
+result_minus = subtract_numbers(5, 3)
+print(result_minus)
